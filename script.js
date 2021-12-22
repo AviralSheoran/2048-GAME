2ad2b51efb9c03b23aefbef1c708faf3788b6a88
AviralSheoran <aviralsheoran.2002@gmail.com>
2021-12-22 19:25:47 +0530

    Swipe right and left

--- script.js
+++ script.js
@@ -28,5 +28,91 @@ document.addEventListener('DOMContentLoaded', () => {
         }else generate()
     }
 
+    // Swipe Right
+    function moveRight() {
+        for (let i = 0; i < 16; i++) {
+            if (i%4 == 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+            
+
+                let filteredRow = row.filter(num => num)
+
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+
+                let newRow = zeros.concat(filteredRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+
+    // Swipe Left
+    function moveLeft() {
+        for (let i = 0; i < 16; i++) {
+            if (i%4 == 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+            
+
+                let filteredRow = row.filter(num => num)
+
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+
+    function combineRow() {
+        for (let i = 0; i <15; i++) {
+            if(squares[i].innerHTML === squares[i+1].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+1].innerHTML = 0
+            }
+        }
+    }
+
+    // Assign Keys
+    function control() {
+        if(e.keyCode === 39) {
+            keyRight()
+        }else if (e.keyCode === 37) {
+            keyLeft()
+        }
+    }
+
+    function keyRight() {
+        moveRight()
+        combineRow()
+        moveRight()
+        generate()
+    }
+
+    function keyLeft() {
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generate()
+    }
 
 })
